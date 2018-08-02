6274f9419cf63906ef8df8d960dff2bfd5192706
wangzy <wzyfly@sina.com>
2018-08-02 21:49:47 +0800

    add beautyeye lib

--- src/com/wangzy/ui/MainUI.java
+++ src/com/wangzy/ui/MainUI.java
@@ -3,12 +3,17 @@ package com.wangzy.ui;
 import java.awt.Dimension;
 import java.awt.EventQueue;
 import java.awt.Toolkit;
-
-import javax.swing.JFrame;
-import javax.swing.JButton;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
 import java.awt.Window.Type;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.UIManager;
+
+import org.jb2011.lnf.beautyeye.BeautyEyeLNFHelper;
+import org.jb2011.lnf.beautyeye.ch3_button.BEButtonUI;
 
 public class MainUI {
 
@@ -18,6 +23,20 @@ public class MainUI {
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
+
+		try {
+
+			BeautyEyeLNFHelper.frameBorderStyle = BeautyEyeLNFHelper.FrameBorderStyle.generalNoTranslucencyShadow;
+			org.jb2011.lnf.beautyeye.BeautyEyeLNFHelper.launchBeautyEyeLNF();
+
+			UIManager.put("RootPane.setupButtonVisible", false);
+
+			BeautyEyeLNFHelper.translucencyAtFrameInactive = true;
+			
+		} catch (Exception e) {
+			// TODO exception
+		}
+
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
@@ -42,28 +61,31 @@ public class MainUI {
 	 */
 	private void initialize() {
 		frmExcel = new JFrame();
+//		frmExcel.setOpacity(0.0f);
 		frmExcel.setType(Type.UTILITY);
 		frmExcel.setResizable(false);
 		frmExcel.setTitle("Excel 工具集(v1.1)");
-		
-		
+
 		int width = 450, height = 416;
 
 		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 
 		int screenWidth = (int) screenSize.getWidth();
 		int screenHeight = (int) screenSize.getHeight();
-		
+
 		frmExcel.setBounds(100, 100, width, height);
 		frmExcel.setLocation(screenWidth / 2 - width / 2, screenHeight / 2 - height / 2);
-		
+
 		frmExcel.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frmExcel.getContentPane().setLayout(null);
-		
+
 		JButton btnNewButton = new JButton("自动抽取文件");
+
+		btnNewButton.setUI(new BEButtonUI().setNormalColor(BEButtonUI.NormalColor.green));
+
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				
+
 				EventQueue.invokeLater(new Runnable() {
 					public void run() {
 						try {
@@ -78,11 +100,12 @@ public class MainUI {
 		});
 		btnNewButton.setBounds(93, 53, 254, 125);
 		frmExcel.getContentPane().add(btnNewButton);
-		
+
 		JButton btnNewButton_1 = new JButton("按列重命名工具");
+		btnNewButton_1.setUI(new BEButtonUI().setNormalColor(BEButtonUI.NormalColor.blue));
 		btnNewButton_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				
+
 				EventQueue.invokeLater(new Runnable() {
 					public void run() {
 						try {
